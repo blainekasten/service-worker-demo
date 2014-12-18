78b824cbaa488d416a4944e9898f46d6e44673f2
blainekasten <blainekasten@gmail.com>
2014-12-17 23:04:38 -0600

    wtf

--- sw.js
+++ sw.js
@@ -30,19 +30,19 @@ this.addEventListener('install', function(event) {
 
 
  //////after service worker is installed, we can fetch our cached assets
-this.addEventListener('fetch', function(event) {
-  console.log('fetch responses!!!!')
-  event.respondWith(
-    caches.match(event.request).then(function(response) {
-      // Cache hit - return response
-      if (response) {
-        return response;
-      }
+//this.addEventListener('fetch', function(event) {
+  //console.log('fetch responses!!!!')
+  //event.respondWith(
+    //caches.match(event.request).then(function(response) {
+      //// Cache hit - return response
+      //if (response) {
+        //return response;
+      //}
 
-      console.log(response);
+      //console.log(response);
 
-      return fetch(event.request);
-    });
-  );
-});
+      //return fetch(event.request);
+    //});
+  //);
+//});
 
