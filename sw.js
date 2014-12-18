f912352bc5bd6b1f1dcdb5625e6345dea63a7afe
blainekasten <blainekasten@gmail.com>
2014-12-17 22:50:03 -0600

    import script

--- sw.js
+++ sw.js
@@ -1,6 +1,6 @@
 console.log('starting service worker!');
 // imports scripts locally or even from other sources
-//importScripts('./javascripts/cache-polyfill');
+importScripts('./javascripts/cache-polyfill');
 console.log('imported script');
 
 
