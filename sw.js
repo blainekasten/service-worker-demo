76ac7177dc676ae6b76b2614c08323bf4e417fb3
blainekasten <blainekasten@gmail.com>
2014-12-17 23:39:54 -0600

    test

--- sw.js
+++ sw.js
@@ -18,10 +18,9 @@ var CACHE_NAME = 'my-site-cache-v3';
 //// this is called during the install 
 this.addEventListener('install', function(event) {
   event.waitUntil(
-    caches.open(CACHE_NAME)
-      .then(function(cache) {
-        return cache.addAll(urlsToCache);
-      })
+    caches.open(CACHE_NAME).then(function(cache) {
+      return cache.addAll(urlsToCache);
+    })
   );
 });
 
@@ -29,13 +28,14 @@ this.addEventListener('install', function(event) {
 
  //////after service worker is installed, we can fetch our cached assets
 this.addEventListener('fetch', function(event) {
-  console.log(event);
   event.respondWith(
     caches.match(event.request).then(function(response) {
       // Cache hit - return response
       if (response) {
         return response;
       }
+
+      console.log(response, event);
 
       return fetch(event.request);
     });
