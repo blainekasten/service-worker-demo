382853d424432d55e51bfbd25203630d36bdeb20
blainekasten <blainekasten@gmail.com>
2015-02-09 20:53:46 -0600

    debug

--- sw.js
+++ sw.js
@@ -70,6 +70,7 @@ self.addEventListener('fetch', function(event) {
       var fetchRequest = event.request.clone();
 
       return fetch(fetchRequest).then(
+        debugger
 
         // when we get the response from the network
         // cache it
